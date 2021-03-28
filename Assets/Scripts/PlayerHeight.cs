3c8cfe9825a129ed71332f86e1283a4ae27f1990
Vicente Vivian <ivcente@gmail.com>
2021-03-28 03:10:14 -0300

    Initial commit

---
+++ Assets/Scripts/PlayerHeight.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerHeight : MonoBehaviour {
+    public Transform player;
+    Text uiText;
+
+    // Start is called before the first frame update
+    void Start() {
+        uiText = GetComponent<Text>();
+    }
+
+    // Update is called once per frame
+    void Update() {
+        // Debug.Log(player.position.y);
+        int height = Mathf.RoundToInt(player.position.y) - 1;
+        uiText.text = height.ToString();
+    }
+}
